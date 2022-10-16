8ab6c39ec1382be9454248a2520dfbbf2cf92761
edeotena <edeotena13@gmail.com>
2022-10-16 18:30:54 +0300

    Users testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  include SessionHelper
+  before_action :require_login
+
+  private
+  def require_login
+    return if signed_in?
+
+    flash[:warning] = 'Login required'
+    redirect_to session_login_url
+  end
+
 end
--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,10 +1,23 @@
 class SessionController < ApplicationController
-  def login
-  end
+  skip_before_action :require_login, only: %i[login create]
+  def login; end
 
   def create
+    user = User.find_by_name params[:user]
+
+    if user && user.password_digest == params[:password]
+      sign_in user
+      #redirect_to base_page
+    else
+      redirect_to session_login_path
+      flash[:warning] = 'Wrong login or password!'
+    end
+
   end
 
   def logout
+    sign_out
+    redirect_to session_login_path
   end
+
 end
